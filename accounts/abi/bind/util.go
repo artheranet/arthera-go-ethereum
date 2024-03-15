0487dd9c4571be41f0f6dce124038443acb8fa00
artheraone <126314488+artheraone@users.noreply.github.com>
2024-03-15 13:15:25 +0200

    accounts/abi/bind: improve WaitMined error handling

--- accounts/abi/bind/util.go
+++ accounts/abi/bind/util.go
@@ -19,6 +19,7 @@ package bind
 import (
 	"context"
 	"errors"
+	"github.com/ethereum/go-ethereum"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -35,14 +36,16 @@ func WaitMined(ctx context.Context, b DeployBackend, tx *types.Transaction) (*ty
 	logger := log.New("hash", tx.Hash())
 	for {
 		receipt, err := b.TransactionReceipt(ctx, tx.Hash())
-		if receipt != nil {
+		if err == nil {
 			return receipt, nil
 		}
-		if err != nil {
-			logger.Trace("Receipt retrieval failed", "err", err)
-		} else {
+
+		if errors.Is(err, ethereum.NotFound) {
 			logger.Trace("Transaction not yet mined")
+		} else {
+			logger.Trace("Receipt retrieval failed", "err", err)
 		}
+
 		// Wait for the next round.
 		select {
 		case <-ctx.Done():
