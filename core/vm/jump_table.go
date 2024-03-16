2bc6ae8973b8b16f6eda0d172cc0ffea9e0e431b
artheraone <126314488+artheraone@users.noreply.github.com>
2024-03-16 09:59:10 +0200

    core/vm: deepcopy jumptable when enabling extra eips

--- core/vm/jump_table.go
+++ core/vm/jump_table.go
@@ -1031,3 +1031,14 @@ func newFrontierInstructionSet() JumpTable {
 
 	return validate(tbl)
 }
+
+func copyJumpTable(source *JumpTable) *JumpTable {
+	dest := *source
+	for i, op := range source {
+		if op != nil {
+			opCopy := *op
+			dest[i] = &opCopy
+		}
+	}
+	return &dest
+}
